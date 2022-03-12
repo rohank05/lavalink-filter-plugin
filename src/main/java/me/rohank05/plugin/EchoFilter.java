b1e0648d72acfd3c188114fd0ea1729fbd2e8be8
rohan <rohan.shuvam@gmail.com>
2022-03-12 21:29:36 +0530

    Added Echo Filter

--- src/main/java/me/rohank05/plugin/EchoFilter.java
+++ src/main/java/me/rohank05/plugin/EchoFilter.java
@@ -5,6 +5,7 @@ import com.sedmelluq.discord.lavaplayer.format.AudioDataFormat;
 import dev.arbjerg.lavalink.api.AudioFilterExtension;
 import dev.arbjerg.lavalink.api.ISocketContext;
 import dev.arbjerg.lavalink.api.PluginEventHandler;
+import me.rohank05.echo.EchoPcmAudioFilter;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +17,7 @@ public class EchoFilter implements AudioFilterExtension {
     private static final Logger log = LoggerFactory.getLogger(EchoFilter.class);
 
     public EchoFilter() {
-        log.info("Hello, world!");
+        log.info("Loading EchoFilter...");
     }
 
 
@@ -27,11 +28,13 @@ public class EchoFilter implements AudioFilterExtension {
 
     @Override
     public FloatPcmAudioFilter build(JSONObject data, AudioDataFormat format, FloatPcmAudioFilter output) {
-        return null;
+        EchoPcmAudioFilter echoPcmAudioFilter = new EchoPcmAudioFilter(output, format.channelCount, format.sampleRate)
+                .setDecay(data.getFloat("decay")).setDelay(data.getDouble("delay"));
+        return echoPcmAudioFilter;
     }
 
     @Override
     public boolean isEnabled(JSONObject data) {
-        return false;
+        return data.getFloat("decay")!= 1.0f || data.getDouble("delay") != 0;
     }
 }
